9b5fa3becea6ebfa8caabe7ab4eb220634de7066
beaudelaaf-star <beaudelaaf@gmail.com>
2026-04-02 17:00:00 +0200

    Update L2.py

---
+++ L2.py
@@ -0,0 +1,51 @@
+import pgzrun 
+from random import randint
+
+WIDTH = 600 
+HEIGHT = 500
+TITLE = "Protect the nest"
+score = 0
+game_over = False
+
+ant1 = Actor ("ant1")
+ant1.pos = 50,280
+ant2 = Actor ("ant2")
+ant2.pos = 290,200
+
+def draw ():
+    screen.fill("green")
+    ant1.draw()
+    ant2.draw()
+    screen.draw.text("score:"+str(score),color = "black",midtop = (300,10))
+
+    if game_over :
+        screen.fill("white")
+        screen.draw.text("Times up your final score is"+str(score),color = "black",midtop = (300,100),fontsize = 40)
+pgzrun.go()
+
+def place_ant2():
+    ant2.x = randint(50,550)
+    ant2.y = randint(50,450)
+    
+
+def update():
+    global score
+    if keyboard.left:
+        ant1.x = ant1.x-2
+
+    if keyboard.down:
+        ant
+
+
+
+
+
+
+
+
+
+
+
+
+
+
